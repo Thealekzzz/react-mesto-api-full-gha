0d785d1e5231327ab0acc61d0dbfba5615200e95
Alex Kuznetsov <kznv.alex@yandex.ru>
2023-08-13 14:27:21 +0300

    fix: upgrade regex for url

--- backend/consts/regexs.js
+++ backend/consts/regexs.js
@@ -1,4 +1,4 @@
-const urlPattern = /^https?:\/\/(www\.)?[A-z0-9\-.]+\.[a-z]{2,3}[A-z0-9\-./]*#?$/;
+const urlPattern = /^https?:\/\/(www\.)?[A-z0-9\-.]+\.[a-z]{2,3}[A-z0-9\-./?=&%]*#?$/;
 
 module.exports = {
   urlPattern,
