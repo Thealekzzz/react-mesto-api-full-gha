7cf42ecf6d9d5d5a2fa276839671ebce1eac01e7
Alex Kuznetsov <kznv.alex@yandex.ru>
2023-08-13 11:42:59 +0300

    refactor: remove useless logs

--- backend/app.js
+++ backend/app.js
@@ -12,11 +12,6 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(requestLogger);
-app.use((req, res, next) => {
-  console.log(req.method, req.url);
-
-  next();
-});
 app.use(checkCors);
 
 app.use('/', indexRouter);
